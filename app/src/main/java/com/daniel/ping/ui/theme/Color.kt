811be6de2b85adda39f8cfffe6ad94dd68d2cb96
Daniel <cariasdaniel261@gmail.com>
2023-04-10 11:53:24 -0600

    style: Theme update

--- app/src/main/java/com/daniel/ping/ui/theme/Color.kt
+++ app/src/main/java/com/daniel/ping/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.daniel.ping.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val RangoonGreen = Color(0xFF171717)
+val Onyx = Color(0xFF121313)
+val UltramarineBlue = Color(0xFF4561EE)
+val White = Color(0xFFFFFFFF)
+val SilverFoil = Color(0xFFAFAFAF)
+val LimeGreen = Color(0xFF35E830)
