a47a990e3128eb38a770b1ca9b44e6d9eacb387b
Daniel <cariasdaniel261@gmail.com>
2023-04-12 22:46:42 -0600

    build: Registration functionality

---
+++ app/src/main/java/com/daniel/ping/ui/activities/ProfileSetupActivity.kt
@@ -0,0 +1,16 @@
+package com.daniel.ping.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.daniel.ping.ui.theme.PingTheme
+
+class ProfileSetupActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PingTheme {
+            }
+        }
+    }
+}
